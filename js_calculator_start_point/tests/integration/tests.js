37a652632f7f3e64e2e684ce8a5851bcbcfb74b9
Chris Haining <chrishaining@yahoo.co.uk>
2019-09-30 21:38:04 +0100

    initial attempts at first two integrated tests. not working yet

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -17,3 +17,21 @@ describe('calculator functionality', function() {
   })
 
 });
+
+// Do the number buttons update the display of the running total?
+//   it('the number buttons should update the display of the running total', function(){
+//     running_total = element(by.css('#running_total'))
+//     element(by.css('#number2')).click();
+//     expect(running_total.getAttribute('value')).to.eventually.equal('2')
+// })
+
+// Do the arithmetical operations update the display with the result of the operation?
+  it('the arithmetical operations should update the display with the result of the operation', function(){
+      
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+
+// Can multiple operations be chained together?
+// Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+// What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
