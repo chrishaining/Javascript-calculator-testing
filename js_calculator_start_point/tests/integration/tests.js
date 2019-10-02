a3dd2f492e5dedfc232bd709cb9fce69d5523c01
Chris Haining <chrishaining@yahoo.co.uk>
2019-10-02 11:39:09 +0100

    completes the integration tests

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -25,18 +25,67 @@ describe('calculator functionality', function() {
   })
 
   // Do the arithmetical operations update the display with the result of the operation?
-  it('the arithmetical operations should update the display with the result of the operation', function(){
-
-    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  xit('the arithmetical operations should update the display with the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    element(by.css('#operator_add')).click()
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click()
+    expect(running_total.getAttribute('value')).to.eventually.equal('6')
   })
 
+  // Can multiple operations be chained together?
+  xit('multiple operations can be chained together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    element(by.css('#operator_add')).click()
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click()
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
+  })
+
+  // Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+  xit('is the output as expected for a negative number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click()
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click()
+    expect(running_total.getAttribute('value')).to.eventually.equal('-1')
+  })
+
+  xit('is the output as expected for a decimal', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click()
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click()
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.5')
+  })
+
+  xit('is the output as expected for a very large number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click()
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click()
+    expect(running_total.getAttribute('value')).to.eventually.equal('98901')
+  })
+
+  // What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
+  it('is the output as expected for a number divided by zero', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_divide')).click()
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click()
+    expect(running_total.getAttribute('value')).to.eventually.equal('ERROR')
+  })
+
+
 });
-
-
-
-
-
-
-// Can multiple operations be chained together?
-// Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
-// What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
